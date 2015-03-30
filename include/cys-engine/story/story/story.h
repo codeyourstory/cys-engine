c1264f9d9a97e57f2e53cb8e0e490c234f2b4b22
Pierre-Emmanuel Lallemant <pe.lallemant@hotmail.fr>
2015-03-30 14:01:25 +0200

    story now uses utils/data/datatype

--- include/cys-engine/story/story/story.h
+++ include/cys-engine/story/story/story.h
@@ -2,24 +2,29 @@
 #define	CYS_ENGINE__STORY_STORY_STORY_H
 
 #include <string>
-#include <cys-engine/utils/macros.h>
+#include <cys-engine/utils/data/datatype.h>
 
 namespace cys {
 namespace story {
   
-  class Story {
+  class Story : data::Entity {
   public: 
     
     Story();
     Story(const std::string &title);
     ~Story();
     
+    ACCESS_DEF(id, Uint32)
     ACCESS_DEF(title, std::string)
     
   private:
-    
+      
+    Uint32 _id;
     std::string _title;
   };
+  
+  FACTORY_DEF(Story)
+  DATA_DEF(Story)
   
 }
 }
