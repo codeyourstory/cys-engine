0fee57ddd77a085ba55c392b52df330d73dc07b5
Pierre-Emmanuel Lallemant <pe.lallemant@hotmail.fr>
2015-03-31 00:38:16 +0200

    some tests were written to test the sqlite3 connection. To continue

--- tests/tests/utils/database/sqlite3/main.cpp
+++ tests/tests/utils/database/sqlite3/main.cpp
@@ -1,15 +1,19 @@
 /*
- Test of utils/data/datatype.h classes
+ Test of utils/database/connections/Sqlite3Connection 
  
  Don't forget to prefix '_' the attributes of the entity ! 
  
  */
 
-#include <cys-engine/utils/data/datatype.h>
+#include <cys-engine/utils/database/connections/Sqlite3Connection.h>
 #include <string>
+#include <string.h>
+#include <unistd.h>
+
 
 using namespace cys;
 using namespace cys::data;
+using namespace cys::database;
 using namespace std;
 
 // definition of the datatype
@@ -22,6 +26,9 @@ public:
   TestEntity();
   ~TestEntity();
     
+  
+  ACCESS_DEF(id, Sint32)
+  
   ACCESS_DEF(u8, Uint8)
   ACCESS_DEF(u16, Uint16)
   ACCESS_DEF(u32, Uint32)
@@ -35,6 +42,8 @@ public:
   ACCESS_DEF(str, string)
   
 private:
+  
+  Sint32 _id;
   
   Uint8 _u8;
   Uint16 _u16;
@@ -69,6 +78,7 @@ TestEntity::~TestEntity()
 
 // Full declaration of the entity
   
+ACCESS_DECL(TestEntity, id, Sint32)
 ACCESS_DECL(TestEntity, u8, Uint8)
 ACCESS_DECL(TestEntity, u16, Uint16)
 ACCESS_DECL(TestEntity, u32, Uint32)
@@ -86,6 +96,8 @@ FACTORY_DECL(TestEntity)
 
 // Now the Datatype
 DATA_DECL(TestEntity, "test_entity", 
+  DATA_DEF_VAR(id,  SINT32)
+  
   DATA_DEF_VAR(u8,  UINT8)
   DATA_DEF_VAR(u16, UINT16)
   DATA_DEF_VAR(u32, UINT32)
@@ -103,18 +115,47 @@ DATA_DECL(TestEntity, "test_entity",
 
 #define ASSERT_OK(val) if(val); else { printf("test_failed line %d of file %s\n", __LINE__, __FILE__);exit(1); }
 
+void setCurrentDir(const char *argv0);
+
 int main(int argc, char** argv)
 {
+  setCurrentDir(argv[0]);
+  
   DATA(TestEntity) myDatatype;
   TestEntity my_entity;
-  
   my_entity._set_datatype(&myDatatype); // don't forget this !
   
-  // 1) we test my_entity.set_X(Y) and my_entity.get_X(Y) 
+  // remove the db if exists
+  system("rm -f db_test");
+  
+  // create the db
+  SQLite3Connection connection("db_test");
+  
+  ASSERT_OK(connection.get_db() != nullptr)
+    
+  // create the schema
+    
+  connection.execute(
+    "CREATE TABLE test_entity(\n"
+    "  id INTEGER PRIMARY KEY,\n"
+    "  u8 INTEGER,\n"
+    "  u16 INTEGER,\n"
+    "  u32 INTEGER,\n"
+    "  u64 INTEGER,\n"
+    "  s8 INTEGER,\n"
+    "  s16 INTEGER,\n"
+    "  s32 INTEGER,\n"
+    "  s64 INTEGER,\n"
+    "  str VARCHAR(100)\n"
+    ");"
+  );
+  
+  // set the entity some values
 #define TEST_ENTITY_SET(varname, val) \
   my_entity.set_##varname(val); \
   ASSERT_OK(*(my_entity.get_##varname()) == val)
   
+  TEST_ENTITY_SET(id, (connection.get_max_id("test_entity") + 1) )
   TEST_ENTITY_SET(u8, 5)
   TEST_ENTITY_SET(u16, 6)
   TEST_ENTITY_SET(u32, 7)
@@ -126,24 +167,56 @@ int main(int argc, char** argv)
   TEST_ENTITY_SET(s64, 99)
   
   TEST_ENTITY_SET(str, "yolo")
-    
-  // 2) we test my_entity.set(X, Y) and my_entity.get(X, Y)
-  #define TEST_ENTITY_DYNAMIC_SET(varname, val, type) \
-  type tmp1_##varname = val;\
-  my_entity.set(#varname, &tmp1_##varname); \
-  ASSERT_OK(*( (type *) my_entity.get(#varname)) == val)
-
-  TEST_ENTITY_DYNAMIC_SET(u8, 89, Uint8)
-  TEST_ENTITY_DYNAMIC_SET(u16, 425, Uint16)
-  TEST_ENTITY_DYNAMIC_SET(u32, 745, Uint32)
-  TEST_ENTITY_DYNAMIC_SET(u64, 8785785, Uint64)
-    
-  TEST_ENTITY_DYNAMIC_SET(s8, 55, Sint8)
-  TEST_ENTITY_DYNAMIC_SET(s16, -73, Sint16)
-  TEST_ENTITY_DYNAMIC_SET(s32, 255, Sint32)
-  TEST_ENTITY_DYNAMIC_SET(s64, 99, Sint64)
+      
+  // insert the entity in the db
+  connection.insert(&myDatatype, &my_entity);
   
-  TEST_ENTITY_DYNAMIC_SET(str, "yolo42", string)
+  // get the entity back
+  TestEntity *from_db = (TestEntity *) connection.load_single(
+    std::string("id = 1"),
+    &myDatatype
+  );
+  
+  ASSERT_OK(from_db != nullptr)
+ 
+#define TEST_ENTITY_VAL(varname, val) \
+    ASSERT_OK(*(my_entity.get_##varname()) == val)
+    
+  TEST_ENTITY_VAL(id, 1)
+  TEST_ENTITY_VAL(u8, 5)
+  TEST_ENTITY_VAL(u16, 6)
+  TEST_ENTITY_VAL(u32, 7)
+  TEST_ENTITY_VAL(u64, 8)
+    
+  TEST_ENTITY_VAL(s8, -42)
+  TEST_ENTITY_VAL(s16, 73)
+  TEST_ENTITY_VAL(s32, -255)
+  TEST_ENTITY_VAL(s64, 99)
+  
+  TEST_ENTITY_VAL(str, "yolo")
+  
   
   return 0;
+}
+
+void setCurrentDir(const char *argv0)
+{
+    int i, l;	
+    char *s;
+
+    s = strdup(argv0);
+    l = strlen(s);
+
+    for(i = l - 1; (i >= 0) && (s[i] != '/'); i--)
+      ;
+
+    if((i >= 0) && (i < (l - 1)))
+            s[i+1] = 0;
+    else;
+
+    chdir(s);
+
+    free(s);
+
+    return;
 }
